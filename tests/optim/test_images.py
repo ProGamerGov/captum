6724f10baef1147f18d930594cef527f0c3d4180
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-11 14:21:33 -0700

    Add PixelImage tests

--- tests/optim/test_images.py
+++ tests/optim/test_images.py
@@ -29,5 +29,35 @@ class TestFFTImage(BaseTest):
         )
 
 
+class TestPixelImage(BaseTest):
+    def test_pixelimage_random(self) -> None:
+        if torch.__version__ == "1.2.0":
+            raise unittest.SkipTest(
+                "Skipping PixelImage random due to insufficient Torch version."
+            )
+        size = (224, 224)
+        channels = 3
+        image_param = images.PixelImage(size=size, channels=channels)
+        assert image_param.image.dim() == 3
+        assert image_param.image.size(0) == channels
+        assert image_param.image.size(1) == size[0]
+        assert image_param.image.size(2) == size[1]
+
+    def test_pixelimage_init(self) -> None:
+        if torch.__version__ == "1.2.0":
+            raise unittest.SkipTest(
+                "Skipping PixelImage init due to insufficient Torch version."
+            )
+        size = (224, 224)
+        channels = 3
+        init_tensor = torch.randn(3, 224, 224)
+        image_param = images.PixelImage(size=size, channels=channels, init=init_tensor)
+        assert image_param.image.dim() == 3
+        assert image_param.image.size(0) == channels
+        assert image_param.image.size(1) == size[0]
+        assert image_param.image.size(2) == size[1]
+        assert torch.all(image_param.image.eq(init_tensor))
+
+
 if __name__ == "__main__":
     unittest.main()
