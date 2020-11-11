79558b26faaa5acb0f31bb83847bbd88f5f9a814
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-11 15:22:36 -0700

    Add model_utils test

---
+++ tests/optim/test_model_utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import unittest
+
+import captum.optim._models.model_utils as model_utils
+import torch
+import torch.nn.functional as F
+from tests.helpers.basic import BaseTest
+
+
+class TestReluLayer(BaseTest):
+    def test_relu_layer(self) -> None:
+        x = torch.randn(1, 3, 4, 4)
+
+        relu_layer = model_utils.ReluLayer()
+        assert torch.all(relu_layer(x).eq(F.relu(x)))
