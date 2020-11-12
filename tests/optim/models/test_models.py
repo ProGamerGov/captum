1153f3942ae0fab6b33f99e17c6fd8f08ca4ea0c
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-12 09:39:54 -0700

    Add different input sizes for testing

--- tests/optim/models/test_models.py
+++ tests/optim/models/test_models.py
@@ -6,11 +6,23 @@ from captum.optim._models.inception_v1 import googlenet
 
 
 class TestInceptionV1(BaseTest):
-    def test_load_and_forward_inceptionv1(self) -> None:
+    def test_load_and_forward_basic_inceptionv1(self) -> None:
         x = torch.randn(1, 3, 224, 224)
         model = googlenet(pretrained=True)
         try:
             output = model(x)
+            test = True
+        except:
+            test = False
+        assert test == True
+
+    def test_load_and_forward_diff_sizes_inceptionv1(self) -> None:
+        x = torch.randn(1, 3, 512, 512)
+        x2 = torch.randn(1, 3, 383, 511)
+        model = googlenet(pretrained=True)
+        try:
+            output = model(x)
+            output2 = model(x2)
             test = True
         except:
             test = False
