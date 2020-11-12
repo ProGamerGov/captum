5558cae0e6fb97128ef6166d367d7d726dda1b6d
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-12 09:36:03 -0700

    Update test_models.py

--- tests/optim/models/test_models.py
+++ tests/optim/models/test_models.py
@@ -6,7 +6,7 @@ from captum.optim._models.inception_v1 import googlenet
 
 
 class TestInceptionV1(BaseTest):
-    def test_inceptionv1(self) -> None:
+    def test_load_and_forward_inceptionv1(self) -> None:
         x = torch.randn(1, 3, 224, 224)
         model = googlenet(pretrained=True)
         try:
