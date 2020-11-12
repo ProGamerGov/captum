6774bed8e56a79d256eabb62d9598b40e0aef612
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-12 09:35:02 -0700

    Remove empty line

--- tests/optim/models/test_models.py
+++ tests/optim/models/test_models.py
@@ -8,7 +8,6 @@ from captum.optim._models.inception_v1 import googlenet
 class TestInceptionV1(BaseTest):
     def test_inceptionv1(self) -> None:
         x = torch.randn(1, 3, 224, 224)
-
         model = googlenet(pretrained=True)
         try:
             output = model(x)
