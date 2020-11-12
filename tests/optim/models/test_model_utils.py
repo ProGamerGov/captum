167762d1d52309cc556cbe13b07af74c54c59590
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-12 09:28:30 -0700

    formatting

--- tests/optim/models/test_model_utils.py
+++ tests/optim/models/test_model_utils.py
@@ -31,7 +31,6 @@ class TestLocalResponseNormLayer(BaseTest):
 class TestReluLayer(BaseTest):
     def test_relu_layer(self) -> None:
         x = torch.randn(1, 3, 4, 4)
-
         relu_layer = model_utils.ReluLayer()
         assert torch.all(relu_layer(x).eq(F.relu(x)))
 
