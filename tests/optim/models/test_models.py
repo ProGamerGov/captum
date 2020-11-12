d8ae49e2adfd8bec8bc7a4afaafc459c907d1aa3
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-12 09:34:07 -0700

    Add model loading and forward test

---
+++ tests/optim/models/test_models.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import unittest
+
+import torch
+from captum.optim._models.inception_v1 import googlenet
+
+
+class TestInceptionV1(BaseTest):
+    def test_inceptionv1(self) -> None:
+        x = torch.randn(1, 3, 224, 224)
+
+        model = googlenet(pretrained=True)
+        try:
+            output = model(x)
+            test = True
+        except:
+            test = False
+        assert test == True
