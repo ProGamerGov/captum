1e03d55603bc35dbe2b4f578bbf421a896f67d52
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-16 10:15:05 -0700

    Add files via upload

--- captum/optim/_core/loss.py
+++ captum/optim/_core/loss.py
@@ -1,229 +1,26 @@
-from abc import ABC, abstractmethod
-
 import torch
 import torch.nn as nn
-
-from captum.optim._utils.typing import ModuleOutputMapping
-
-
-class Loss(ABC):
-    r"""
-    Abstract Class to describe loss.
-    """
-
-    def __init__(self, target: nn.Module):
-        super(Loss, self).__init__()
-        self.target = target
-
-    @abstractmethod
-    def __call__(self, x):
-        pass
-
-    def get_neuron_pos(self, H, W, x=None, y=None):
-        if x is None:
-            _x = W // 2
-        else:
-            assert x < W
-            _x = x
-
-        if y is None:
-            _y = H // 2
-        else:
-            assert y < W
-            _y = y
-        return _x, _y
+import torch.nn.functional as F
 
 
-class ChannelActivation(Loss):
-    """
-    Maximize activations at the target layer and target channel.
-    """
-
-    def __init__(self, target: nn.Module, channel_index: int):
-        super(Loss, self).__init__()
-        self.target = target
-        self.channel_index = channel_index
-
-    def __call__(self, targets_to_values: ModuleOutputMapping) -> torch.Tensor:
-        activations = targets_to_values[self.target]
-        assert activations is not None
-        # ensure channel_index is valid
-        assert self.channel_index < activations.shape[1]
-        # assume NCHW
-        # NOTE: not necessarily true e.g. for Linear layers
-        # assert len(activations.shape) == 4
-        return activations[:, self.channel_index, ...]
 
 
-class NeuronActivation(Loss):
-    def __init__(
-        self, target: nn.Module, channel_index: int, x: int = None, y: int = None
-    ):
-        super(Loss, self).__init__()
-        self.target = target
-        self.channel_index = channel_index
-        self.x = x
-        self.y = y
+def test_vec(target, vec):
+    vec = vec.reshape((1, -1, 1, 1))
+    
+    target = target[:, 21, 7, 7]#y:y+1, x:x+1
+    
+    print(target.size(), vec.size())
+    #print(target.reshape((1, -1, 1, 1)))
+       
 
-        # ensure channel_index will be valid
-        assert self.channel_index < self.target.out_channels
-
-    def _call__(self, targets_to_values: ModuleOutputMapping) -> torch.Tensor:
-        activations = targets_to_values[self.target]
-        assert activations is not None
-        assert len(activations.shape) == 4  # assume NCHW
-        _x, _y = self.get_neuron_pos(
-            activations.size(2), activations.size(3), self.x, self.y
-        )
-
-        return activations[:, self.channel_index, _x, _y]
-
-
-class DeepDream(Loss):
-    """
-    Maximize 'interestingness' at the target layer.
-    Mordvintsev et al., 2015.
-    """
-
-    def __call__(self, targets_to_values: ModuleOutputMapping) -> torch.Tensor:
-        activations = targets_to_values[self.target]
-        return activations ** 2
-
-
-class TotalVariation(Loss):
-    """
-    Total variation denoising penalty for activations.
-    See Simonyan, et al., 2014.
-    """
-
-    def _call__(self, targets_to_values: ModuleOutputMapping) -> torch.Tensor:
-        activations = targets_to_values[self.target]
-        x_diff = activations[..., 1:, :] - activations[..., :-1, :]
-        y_diff = activations[..., :, 1:] - activations[..., :, :-1]
-        return torch.sum(torch.abs(x_diff)) + torch.sum(torch.abs(y_diff))
-
-
-class L1(Loss):
-    """
-    L1 norm of the target layer, generally used as a penalty.
-    """
-
-    def __init__(self, target: nn.Module, constant: float = 0):
-        super(Loss, self).__init__()
-        self.target = target
-        self.constant = constant
-
-    def _call__(self, targets_to_values: ModuleOutputMapping) -> torch.Tensor:
-        activations = targets_to_values[self.target]
-        return torch.abs(activations - self.constant).sum()
-
-
-class L2(Loss):
-    """
-    L2 norm of the target layer, generally used as a penalty.
-    """
-
-    def __init__(self, target: nn.Module, constant: float = 0, epsilon: float = 1e-6):
-        self.target = target
-        self.constant = constant
-        self.epsilon = epsilon
-
-    def _call__(self, targets_to_values: ModuleOutputMapping) -> torch.Tensor:
-        activations = targets_to_values[self.target]
-        activations = (activations - self.constant).sum()
-        return torch.sqrt(self.epsilon + activations)
-
-
-class Diversity(Loss):
-    """
-    Use a cosine similarity penalty to extract features from a polysemantic neuron.
-    Olah, Mordvintsev & Schubert, 2017.
-    https://distill.pub/2017/feature-visualization/#diversity
-    """
-
-    def _call__(self, targets_to_values: ModuleOutputMapping) -> torch.Tensor:
-        activations = targets_to_values[self.target]
-        return -sum(
-            [
-                sum(
-                    [
-                        (
-                            torch.cosine_similarity(
-                                activations[j].view(1, -1), activations[i].view(1, -1)
-                            )
-                        ).sum()
-                        for i in range(activations.size(0))
-                        if i != j
-                    ]
-                )
-                for j in range(activations.size(0))
-            ]
-        ) / activations.size(0)
+ 
+    #quit()
+    return torch.cosine_similarity(target[None, None, None], vec)
     
     
-class Direction(Loss):
-    """
-    Visualize a neuron direction.
-    """
-
-    def __init__(self, target: nn.Module, vec: torch.Tensor):
-        super(Loss, self).__init__()
-        self.target = target
-        self.direction = vec.reshape((1, -1, 1, 1))
-
-    def __call__(self, targets_to_values: ModuleOutputMapping) -> torch.Tensor:
-        activations = targets_to_values[self.target]
-        return torch.cosine_similarity(self.direction, activations)
-
-
-class DirectionNeuron(Loss):
-    """
-    Visualize a direction.
-    """
-
-    def __init__(
-        self,
-        target: nn.Module,
-        vec: torch.Tensor,
-        channel_index: int,
-        x: int = None,
-        y: int = None,
-    ):
-        super(Loss, self).__init__()
-        self.target = target
-        self.direction = vec.reshape((1, -1, 1, 1))
-        self.channel_index = channel_index
-        self.x = x
-        self.y = y
-
-    def __call__(self, targets_to_values: ModuleOutputMapping) -> torch.Tensor:
-        activations = targets_to_values[self.target]
-
-        _x, _y = self.get_neuron_pos(
-            activations.size(2), activations.size(3), self.x, self.y
-        )
-        activations = activations[:, self.channel_index, _x, _y]
-        return torch.cosine_similarity(self.direction, activations[None, None, None])
-
-
-class TensorDirection(Loss):
-    """
-    Visualize a tensor direction.
-    """
-
-    def __init__(self, target: nn.Module, vec: torch.Tensor):
-        super(Loss, self).__init__()
-        self.target = target
-        self.direction = vec
-
-    def __call__(self, targets_to_values: ModuleOutputMapping) -> torch.Tensor:
-        activations = targets_to_values[self.target]
-
-        H_vec, W_vec = self.direction.size(2), self.direction.size(3)
-        H_activ, W_activ = activations.size(2), activations.size(3)
-
-        H = (H_activ - W_vec) // 2
-        W = (W_activ - W_vec) // 2
-
-        activations = activations[:, :, H : H + H_vec, W : W + W_vec]
-        return torch.cosine_similarity(self.direction, activations)
+vec = torch.rand(512) * 1000
+target = torch.randn(1,512,15,15) 
+out = test_vec(target, vec)
+    
+print(out)
