4fd958fc9bf99c28e2ae2bfe8afdfe2fde750d8a
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-20 16:16:31 -0700

    Add more type hints

--- captum/optim/_models/inception_v1.py
+++ captum/optim/_models/inception_v1.py
@@ -10,7 +10,9 @@ GS_SAVED_WEIGHTS_URL = (
 )
 
 
-def googlenet(pretrained=False, progress=True, model_path=None, **kwargs):
+def googlenet(
+    pretrained: bool = False, progress: bool = True, model_path: str = None, **kwargs
+):
     r"""GoogLeNet (also known as Inception v1 & Inception 5h) model architecture from
     `"Going Deeper with Convolutions" <http://arxiv.org/abs/1409.4842>`_.
     Args:
@@ -49,11 +51,16 @@ def googlenet(pretrained=False, progress=True, model_path=None, **kwargs):
 
 # Better version of Inception V1/GoogleNet for Inception5h
 class InceptionV1(nn.Module):
-    def __init__(self, out_features=1008, aux_logits=False, transform_input=False):
+    def __init__(
+        self,
+        out_features: int = 1008,
+        aux_logits: bool = False,
+        transform_input: bool = False,
+    ) -> None:
         super(InceptionV1, self).__init__()
         self.aux_logits = aux_logits
         self.transform_input = transform_input
-        lrn_vals = (9, 9.99999974738e-05, 0.5, 1)
+        lrn_vals = (9, 9.99999974738e-05, 0.5, 1.0)
 
         self.conv1 = nn.Conv2d(
             in_channels=3,
@@ -114,14 +121,14 @@ class InceptionV1(nn.Module):
         self.drop = nn.Dropout(0.4000000059604645)
         self.fc = nn.Linear(1024, out_features)
 
-    def _transform_input(self, x):
+    def _transform_input(self, x: torch.Tensor) -> torch.Tensor:
         if self.transform_input:
             assert x.min() >= 0.0 and x.max() <= 1.0
             x = x * 255 - 117
             x = x.clone()[:, [2, 1, 0]] if x.dim() == 4 else x  # RGB to BGR
         return x
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self._transform_input(x)
         x = self.conv1(x)
         x = self.conv1_relu(x)
@@ -171,8 +178,15 @@ class InceptionV1(nn.Module):
 
 class InceptionModule(nn.Module):
     def __init__(
-        self, in_channels, c1x1, c3x3reduce, c3x3, c5x5reduce, c5x5, pool_proj
-    ):
+        self,
+        in_channels: int,
+        c1x1: int,
+        c3x3reduce,
+        c3x3: int,
+        c5x5reduce: int,
+        c5x5: int,
+        pool_proj: int,
+    ) -> None:
         super(InceptionModule, self).__init__()
         self.conv_1x1 = nn.Conv2d(
             in_channels=in_channels,
@@ -235,7 +249,7 @@ class InceptionModule(nn.Module):
         )
         self.pool_proj_relu = model_utils.ReluLayer()
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         c1x1 = self.conv_1x1(x)
         c1x1 = self.conv_1x1_relu(c1x1)
 
@@ -257,7 +271,7 @@ class InceptionModule(nn.Module):
 
 
 class AuxBranch(nn.Module):
-    def __init__(self, in_channels=508, out_features=1008):
+    def __init__(self, in_channels: int = 508, out_features: int = 1008) -> None:
         super(AuxBranch, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d((4, 4))
         self.loss_conv = nn.Conv2d(
@@ -276,7 +290,7 @@ class AuxBranch(nn.Module):
             in_features=1024, out_features=out_features, bias=True
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.avg_pool(x)
         x = self.loss_conv(x)
         x = self.loss_conv_relu(x)
