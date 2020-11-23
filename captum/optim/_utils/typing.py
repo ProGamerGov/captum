1294b647b446381eb3290b456fe673cfef9d8bb1
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-23 10:38:07 -0700

    Update typing.py

--- captum/optim/_utils/typing.py
+++ captum/optim/_utils/typing.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Callable, Dict, Iterable, Optional, Union
+from typing import Callable, Dict, Iterable, Optional, Tuple, Union
 
 from torch import Tensor
 from torch.nn import Module
@@ -32,3 +32,5 @@ ModuleOutputMapping = Dict[Module, Optional[Tensor]]
 StopCriteria = Callable[[int, Objective, Optimizer], bool]
 LossFunction = Callable[[ModuleOutputMapping], Tensor]
 SingleTargetLossFunction = Callable[[Tensor], Tensor]
+
+HookFuncArg = Callable[[Module, Union[Tuple[Tensor, ...], Tensor], Union[Tuple[Tensor, ...], Tensor]], Optional[Tensor]]
