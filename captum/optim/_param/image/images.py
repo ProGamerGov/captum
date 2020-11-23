0ad63e8791f3f373e9a776ab747bc2a693905d5c
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-11-23 09:53:40 -0700

    Update images.py

--- captum/optim/_param/image/images.py
+++ captum/optim/_param/image/images.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-from typing import List, Optional, Tuple
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
