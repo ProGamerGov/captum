3e4e40ed6e839465573dd614b176b99a7cecb567
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-12-04 13:16:46 -0700

    Update numpy_image.py

--- tests/optim/helpers/numpy_image.py
+++ tests/optim/helpers/numpy_image.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def rfft2d_freqs(height: int, width: int) -> np.ndarray:
-    """Computes 2D spectrum frequencies."""
+    """NumPy version of rfft2d_freqs"""
     fy = np.fft.fftfreq(height)[:, None]
     # on odd input dimensions we need to keep one additional frequency
     wadd = 2 if width % 2 == 1 else 1
