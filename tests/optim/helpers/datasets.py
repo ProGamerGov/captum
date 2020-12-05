d894fb1eee5830a3566e001c6e24e72755468026
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-12-05 13:02:34 -0700

    Update datasets.py

--- tests/optim/helpers/datasets.py
+++ tests/optim/helpers/datasets.py
@@ -6,7 +6,8 @@ import torch
 class ImageTestDataset(torch.utils.data.Dataset):
     def __init__(self, tensors: List[torch.Tensor]) -> None:
         assert all(t.size(0) == 1 for t in tensors if t.dim() == 4)
-        tensors = [t.squeeze(0) for t in tensors]
+        t_squeeze = lambda x: x.squeeze(0) if x.dim() == 4 else x
+        tensors = [t_squeeze(t) for t in tensors]
         self.tensors = tensors
 
     def __getitem__(self, idx: int) -> torch.Tensor:
