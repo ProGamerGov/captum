a5c9943b5ab1e7300d68a75b1f6e7978cdbbf54b
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-12-05 10:53:12 -0700

    Add files via upload

--- captum/optim/_misc/dataset.py
+++ captum/optim/_misc/dataset.py
@@ -5,12 +5,21 @@ def rgb_cov(tensor: torch.Tensor) -> torch.Tensor:
     """
     RGB Cov
     """
+
     tensor = tensor.reshape(-1, tensor.size(1))
     tensor = tensor - tensor.mean(0, keepdim=True)
     return 1 / (tensor.size(0) - 1) * tensor.T @ tensor
 
 
-def dataset_color_decorrelation_matrix(loader: torch.utils.data.DataLoader) -> torch.Tensor:
+def dataset_color_correlation_matrix(
+    loader: torch.utils.data.DataLoader,
+) -> torch.Tensor:
+    """
+    Calculate the color correlation matrix for a dataset.
+    The color correlation matrix can then used in color decorrelation
+    transforms for models trained on the dataset.
+    """
+
     cov_mtx = 0
     for images, _ in loader:
         for b in range(images.size(0)):
