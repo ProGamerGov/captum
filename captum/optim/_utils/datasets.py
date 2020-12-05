8515c481807b8d0a7bee9999f21b69be187589d9
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-12-05 14:17:22 -0700

    Add files via upload

--- captum/optim/_utils/datasets.py
+++ captum/optim/_utils/datasets.py
@@ -6,9 +6,36 @@ def image_cov(tensor: torch.Tensor) -> torch.Tensor:
     Calculate a tensor's RGB covariance matrix
     """
 
-    tensor = tensor.reshape(-1, tensor.size(1))
+    tensor = tensor.reshape(-1, 3)
     tensor = tensor - tensor.mean(0, keepdim=True)
     return 1 / (tensor.size(0) - 1) * tensor.T @ tensor
+
+
+def dataset_cov_matrix(loader: torch.utils.data.DataLoader) -> torch.Tensor:
+    """
+    Calculate the covariance matrix for an image dataset.
+    """
+
+    cov_mtx = 0.0
+    for images, _ in loader:
+        assert images.dim() == 4
+        for b in range(images.size(0)):
+            cov_mtx = cov_mtx + image_cov(images[b].permute(1, 2, 0))
+    return cov_mtx
+
+
+def dataset_cov_to_klt(
+    cov_mtx: torch.Tensor, normalize: bool = False, epsilon: float = 1e-10
+) -> torch.Tensor:
+    """
+    Convert a cov matrix to a klt matrix.
+    """
+
+    U, S, V = torch.svd(cov_mtx)
+    svd_sqrt = U @ torch.diag(torch.sqrt(S + epsilon))
+    if normalize:
+        svd_sqrt / torch.max(torch.norm(svd_sqrt, dim=0))
+    return svd_sqrt
 
 
 def dataset_klt_matrix(
@@ -21,16 +48,6 @@ def dataset_klt_matrix(
     transforms for models trained on the dataset.
     """
 
-    cov_mtx = 0
-    for images, _ in loader:
-        for b in range(images.size(0)):
-            cov_mtx += image_cov(images[b].permute(1, 2, 0))
-
-    cov_mtx = cov_mtx / len(loader.dataset)
-
-    U, S, V = torch.svd(cov_mtx)
-    epsilon = 1e-10
-    svd_sqrt = U @ torch.diag(torch.sqrt(S + epsilon))
-    if normalize:
-        svd_sqrt / torch.max(torch.norm(svd_sqrt, dim=0))
-    return svd_sqrt
+    cov_mtx = dataset_cov_matrix(loader)
+    cov_mtx = cov_mtx / len(loader.dataset)  # type: ignore
+    return dataset_cov_to_klt(cov_mtx, normalize)
