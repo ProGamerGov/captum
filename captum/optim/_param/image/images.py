ac7c71fb09d6ef67dd36f732c275e44edd0ebfa6
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-12-19 10:12:28 -0700

    Update images.py

--- captum/optim/_param/image/images.py
+++ captum/optim/_param/image/images.py
@@ -395,7 +395,7 @@ class SharedImage(ImageParameterization):
                 offset = [([0] * (4 - len(offset))) + list(offset)] * n
         else:
             offset = [[offset] * 4] * n
-        offset = [list(v) for v in offset_vals]
+        offset = [list(v) for v in offset]
         assert all([all([type(o) is int for o in v]) for v in offset])
         return offset
 
