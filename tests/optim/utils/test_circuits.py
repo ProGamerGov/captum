cd6673e7259ae0d3249fcb4f56f46969781b918b
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-12-20 14:51:44 -0700

    Update test_circuits.py

--- tests/optim/utils/test_circuits.py
+++ tests/optim/utils/test_circuits.py
@@ -5,7 +5,7 @@ import torch
 
 import captum.optim._utils.circuits as circuits
 from captum.optim._models.inception_v1 import googlenet
-from tests.helpers.basic import BaseTest
+from tests.helpers.basic import BaseTest, assertTensorAlmostEqual
 
 
 class TestGetExpandedWeights(BaseTest):
@@ -19,6 +19,14 @@ class TestGetExpandedWeights(BaseTest):
             model, model.mixed4c, model.mixed4d
         )
         self.assertTrue(torch.is_tensor(output_tensor))
+
+
+class TestRemoveConstantPad(BaseTest):
+    def test_remove_constant_pad(self) -> None:
+        x = torch.randn(1, 4, 5, 5)
+        x_padded = torch.nn.functional.pad(a, (4, 4, 4, 4), "constant", value=1.2)
+        x_out = circuits.remove_constant_pad(x)
+        assertTensorAlmostEqual(self, x_out, x)
 
 
 if __name__ == "__main__":
