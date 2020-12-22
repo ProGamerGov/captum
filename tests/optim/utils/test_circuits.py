1a9ad3c52e5fd88d6a92da71c26fe2afdfb22933
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-12-22 09:31:38 -0700

    Update test_circuits.py

--- tests/optim/utils/test_circuits.py
+++ tests/optim/utils/test_circuits.py
@@ -16,9 +16,34 @@ class TestGetExpandedWeights(BaseTest):
             )
         model = googlenet(pretrained=True)
         output_tensor = circuits.get_expanded_weights(
-            model, model.mixed4c, model.mixed4d
+            model, model.mixed3a, model.mixed3b
         )
         self.assertTrue(torch.is_tensor(output_tensor))
+        self.assertEqual(list(output_tensor.shape), [480, 256, 28, 28])
+
+    def test_get_expanded_weights_crop_int(self) -> None:
+        if torch.__version__ == "1.2.0":
+            raise unittest.SkipTest(
+                "Skipping get_expanded_weights crop test due to insufficient Torch"
+                + " version."
+            )
+        model = googlenet(pretrained=True)
+        output_tensor = circuits.get_expanded_weights(
+            model, model.mixed3a, model.mixed3b, 5
+        )
+        self.assertEqual(list(output_tensor.shape), [480, 256, 5, 5])
+
+    def test_get_expanded_weights_crop_two_int(self) -> None:
+        if torch.__version__ == "1.2.0":
+            raise unittest.SkipTest(
+                "Skipping get_expanded_weights two int crop test due to insufficient"
+                + " Torch version."
+            )
+        model = googlenet(pretrained=True)
+        output_tensor = circuits.get_expanded_weights(
+            model, model.mixed3a, model.mixed3b, (5, 5)
+        )
+        self.assertEqual(list(output_tensor.shape), [480, 256, 5, 5])
 
 
 if __name__ == "__main__":
