602356b466d0da47d0a88a82c6a26a6ed11f7e49
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-12-22 16:08:01 -0700

    Update common.py

--- captum/optim/_utils/image/common.py
+++ captum/optim/_utils/image/common.py
@@ -1,4 +1,4 @@
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 import torch
 
@@ -78,3 +78,47 @@ def nchannels_to_rgb(x: torch.Tensor, warp: bool = True) -> torch.Tensor:
     return (rgb / (1e-4 + torch.norm(rgb, dim=1, keepdim=True))) * torch.norm(
         x, dim=1, keepdim=True
     )
+
+
+def tensor_heatmap(
+    tensor: torch.Tensor,
+    colors: List[str] = ["0571b0", "92c5de", "f7f7f7", "f4a582", "ca0020"],
+) -> torch.Tensor:
+    """
+    Create a color heatmap of an input weight tensor.
+    By default red represents excitatory values,
+    blue represents inhibitory values, and white represents
+    no excitation or inhibition.
+    """
+
+    assert tensor.dim() == 2
+    assert len(colors) == 5
+
+    def get_color(x: str) -> torch.Tensor:
+        def hex2base10(x: str) -> float:
+            return int(x, 16) / 255.0
+
+        return torch.tensor(
+            [hex2base10(x[0:2]), hex2base10(x[2:4]), hex2base10(x[4:6])]
+        )
+
+    def color_scale(x: torch.Tensor) -> torch.Tensor:
+        if x < 0:
+            x = -x
+            if x < 0.5:
+                x = x * 2
+                return (1 - x) * get_color(colors[2]) + x * get_color(colors[1])
+            else:
+                x = (x - 0.5) * 2
+                return (1 - x) * get_color(colors[1]) + x * get_color(colors[0])
+        else:
+            if x < 0.5:
+                x = x * 2
+                return (1 - x) * get_color(colors[2]) + x * get_color(colors[3])
+            else:
+                x = (x - 0.5) * 2
+                return (1 - x) * get_color(colors[3]) + x * get_color(colors[4])
+
+    return torch.stack(
+        [torch.stack([color_scale(x) for x in t]) for t in tensor]
+    ).permute(2, 0, 1)
