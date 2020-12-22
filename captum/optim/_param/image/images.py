d96ac5a869e11862a94be74aede10da73db21881
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-12-22 15:44:35 -0700

    Update images.py

--- captum/optim/_param/image/images.py
+++ captum/optim/_param/image/images.py
@@ -13,7 +13,6 @@ except (ImportError, AssertionError):
     print("The Pillow/PIL library is required to use Captum's Optim library")
 
 from captum.optim._param.image.transform import SymmetricPadding, ToRGB
-from captum.optim._utils.models import pad_reflective_a4d
 from captum.optim._utils.typing import InitSize, SquashFunc
 
 
