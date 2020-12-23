33ed528c2509642e81e19d895be0e4369faa12de
ProGamerGov <ProGamerGov@users.noreply.github.com>
2020-12-22 17:20:29 -0700

    Update common.py

--- tests/optim/utils/image/common.py
+++ tests/optim/utils/image/common.py
@@ -1,34 +1,34 @@
 #!/usr/bin/env python3
 import unittest
 
-from captum.optim._utils.images import get_neuron_pos
+import captum.optim._utils.image.common as common
 
 
 class TestGetNeuronPos(unittest.TestCase):
     def test_get_neuron_pos_hw(self) -> None:
         W, H = 128, 128
-        x, y = get_neuron_pos(H, W)
+        x, y = common.get_neuron_pos(H, W)
 
         self.assertEqual(x, W // 2)
         self.assertEqual(y, H // 2)
 
     def test_get_neuron_pos_xy(self) -> None:
         W, H = 128, 128
-        x, y = get_neuron_pos(H, W, 5, 5)
+        x, y = common.get_neuron_pos(H, W, 5, 5)
 
         self.assertEqual(x, 5)
         self.assertEqual(y, 5)
 
     def test_get_neuron_pos_x_none(self) -> None:
         W, H = 128, 128
-        x, y = get_neuron_pos(H, W, 5, None)
+        x, y = common.get_neuron_pos(H, W, 5, None)
 
         self.assertEqual(x, 5)
         self.assertEqual(y, H // 2)
 
     def test_get_neuron_pos_none_y(self) -> None:
         W, H = 128, 128
-        x, y = get_neuron_pos(H, W, None, 5)
+        x, y = common.get_neuron_pos(H, W, None, 5)
 
         self.assertEqual(x, W // 2)
         self.assertEqual(y, 5)
