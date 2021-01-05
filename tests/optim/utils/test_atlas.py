92bc6a102920fa428ce8441ed132d67ad14dead4
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-01-05 12:13:41 -0700

    Create test_atlas.py

---
+++ tests/optim/utils/test_atlas.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import unittest
+
+import torch
+
+import captum.optim._utils.atlas as atlas
+from tests.helpers.basic import BaseTest
+
+
+if __name__ == "__main__":
+    unittest.main()
