3f560da395533b2b435d21d4ccd5f1e21957cb65
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-01-09 10:48:51 -0700

    Update common_helpers.py

--- tests/optim/helpers/common_helpers.py
+++ tests/optim/helpers/common_helpers.py
@@ -1,12 +1,5 @@
-def check_layer_does_not_exist(self, model, layer) -> None:
+def check_layer_not_in_model(self, model, layer) -> None:
     for name, child in model._modules.items():
         if child is not None:
             self.assertNotIsInstance(child, layer)
-            check_layer_does_not_exist(self, child, layer)
-
-
-def check_layer_exists(self, model, layer) -> None:
-    for name, child in model._modules.items():
-        if child is not None:
-            self.assertNotIsInstance(child, layer)
-            check_layer_exists(self, child, layer)
+            check_layer_not_in_model(self, child, layer)
