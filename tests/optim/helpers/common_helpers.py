6b2a2102517295ea15a3efb36e444557a6ce31c2
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-01-09 10:54:53 -0700

    Delete common_helpers.py

--- tests/optim/helpers/common_helpers.py
+++
@@ -1,5 +0,0 @@
-def check_layer_not_in_model(self, model, layer) -> None:
-    for name, child in model._modules.items():
-        if child is not None:
-            self.assertNotIsInstance(child, layer)
-            check_layer_not_in_model(self, child, layer)
