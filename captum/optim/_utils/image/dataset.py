0457cee3cdebbce9454a53dd0847e25feb4c02af
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-02-16 12:50:24 -0700

    Update dataset.py

--- captum/optim/_utils/image/dataset.py
+++ captum/optim/_utils/image/dataset.py
@@ -1,5 +1,5 @@
 import os
-from typing import Dict, List, Optional, cast
+from typing import List, Optional, Tuple, Union, cast
 
 import torch
 
@@ -84,6 +84,37 @@ def dataset_klt_matrix(
     return cov_matrix_to_klt(cov_mtx, normalize)
 
 
+def find_pos_attr(
+    logit_activ: torch.Tensor,
+    target_activ: torch.Tensor,
+    y: Optional[Union[int, torch.Tensor]] = None,
+    x: Optional[Union[int, torch.Tensor]] = None,
+) -> torch.Tensor:
+    assert x is not None and y is not None or x is None and y is None
+
+    zeros = torch.nn.Parameter(torch.zeros_like(logit_activ))
+    target_zeros = torch.zeros_like(target_activ)
+
+    if x is not None and y is not None:
+        target_zeros[..., y, x] = target_activ[..., y, x]
+    else:
+        target_zeros = target_activ
+
+    grad_one = torch.autograd.grad(
+        outputs=[logit_activ],
+        inputs=[target_activ],
+        grad_outputs=[zeros],
+        create_graph=True,
+    )[0]
+    logit_attr = torch.autograd.grad(
+        outputs=[grad_one],
+        inputs=zeros,
+        grad_outputs=[target_zeros],
+        create_graph=True,
+    )[0]
+    return torch.argsort(-logit_attr[0])
+
+
 def capture_activation_samples(
     loader: torch.utils.data.DataLoader,
     model: torch.nn.Module,
@@ -93,8 +124,10 @@ def capture_activation_samples(
     num_images: Optional[int] = None,
     samples_per_image: int = 1,
     input_device: torch.device = torch.device("cpu"),
+    collect_attributions: bool = False,
+    logit_target: Optional[torch.nn.Module] = None,
     show_progress: bool = False,
-) -> Dict[str, torch.Tensor]:
+):
     """
     Capture randomly sampled activations for an image dataset from one or multiple
     target layers.
@@ -113,16 +146,32 @@ def capture_activation_samples(
             is 1 sample per image.
         input_device (torch.device, optional): The device to use for model
             inputs.
+        collect_attributions (bool, optional): Whether or not to collect attributions
+            for samples.
+        logit_target (nn.Module, optional): The final layer in the model that
+            determines the classes.
         show_progress (bool, optional): Whether or not to show progress.
     """
 
-    def random_sample(activations: torch.Tensor) -> torch.Tensor:
+    if collect_attributions:
+        logit_target == list(model.children())[len(list(model.children())) - 1 :][
+            0
+        ] if logit_target is None else logit_target
+        targets += [cast(torch.nn.Module, logit_target)]
+
+    def random_sample(
+        activations: torch.Tensor,
+        logit_activ: torch.Tensor,
+    ) -> Tuple[List[torch.Tensor], List]:
         """
         Randomly sample H & W dimensions of activations with 4 dimensions.
         """
         assert activations.dim() == 4 or activations.dim() == 2
 
-        rnd_samples = []
+        if collect_attributions:
+            sample_attributions: List = []
+
+        activation_samples = []
         for i in range(samples_per_image):
             for b in range(activations.size(0)):
                 if activations.dim() == 4:
@@ -130,13 +179,20 @@ def capture_activation_samples(
                     y = torch.randint(low=1, high=h - 1, size=[1])
                     x = torch.randint(low=1, high=w - 1, size=[1])
                     activ = activations[b, :, y, x]
+                    if collect_attributions:
+                        attr = find_pos_attr(activations[b : b + 1], logit_activ, y, x)
                 elif activations.dim() == 2:
                     activ = activations[b].unsqueeze(1)
-                rnd_samples.append(activ)
-        return rnd_samples
+                    if collect_attributions:
+                        attr = find_pos_attr(activations[b : b + 1], logit_activ)
+                activation_samples.append(activ.detach())
+                if collect_attributions:
+                    sample_attributions.append(attr.detach())
+        return activation_samples, sample_attributions
 
     if target_names is None:
         target_names = ["target" + str(i) + "_" for i in range(len(targets))]
+
     assert len(target_names) == len(targets)
     assert os.path.isdir(sample_dir)
 
@@ -147,21 +203,35 @@ def capture_activation_samples(
         pbar = tqdm(total=total, unit=" images")
 
     image_count, batch_count = 0, 0
-    with torch.no_grad():
+    with torch.set_grad_enabled(collect_attributions):
         for inputs, _ in loader:
             inputs = inputs.to(input_device)
             image_count += inputs.size(0)
             batch_count += 1
 
-            target_activ_dict = collect_activations(model, targets, inputs)
+            if collect_attributions:
+                target_activ_dict = collect_activations(model, targets, inputs)
+                logit_activ = target_activ_dict[logit_target]
+                del target_activ_dict[logit_target]
+            else:
+                target_activ_dict = collect_activations(model, targets, inputs)
+                logit_activ = None
 
-            [
+            for t, n in zip(target_activ_dict, target_names):
+                sample_tensors = random_sample(target_activ_dict[t], logit_activ)
                 torch.save(
-                    random_sample(target_activ_dict[t]),
-                    os.path.join(sample_dir, +n + "_" + str(batch_count) + ".pt"),
+                    sample_tensors[0],
+                    os.path.join(sample_dir, n + "_" + str(batch_count) + ".pt"),
                 )
-                for t, n in zip(target_activ_dict, target_names)
-            ]
+                if collect_attributions:
+                    torch.save(
+                        sample_tensors[1],
+                        os.path.join(
+                            sample_dir,
+                            n + "_attributions_" + str(batch_count) + ".pt",
+                        ),
+                    )
+
             del target_activ_dict
 
             if show_progress:
@@ -173,43 +243,3 @@ def capture_activation_samples(
 
     if show_progress:
         pbar.close()
-
-
-def consolidate_samples(
-    sample_dir: str = "samples", sample_basename: str = "", show_progress: bool = False
-) -> torch.Tensor:
-    """
-    Combine samples collected from capture_activation_samples into a single
-    tensor with a shape of [n_samples, n_channels].
-
-    Args:
-        sample_dir (str): The directory where activation samples where saved.
-        sample_basename (str, optional): If samples from different layers are
-            present in sample_dir, then you can use samples from only a
-            specific layer by specifying the basename that samples of the same
-            layer share.
-        show_progress (bool, optional): Whether or not to show progress.
-    Returns:
-        sample_tensor (torch.Tensor): A tensor containing all the specified
-            sample tensors with a shape of [n_samples, n_channels].
-    """
-
-    samples = []
-    tensor_samples = [
-        os.path.join(sample_dir, name)
-        for name in os.listdir(sample_dir)
-        if sample_basename.lower() in name.lower()
-        and os.path.isfile(os.path.join(sample_dir, name))
-    ]
-
-    if show_progress:
-        pbar = tqdm(total=len(tensor_samples), unit=" sample batches collected")
-    for file in tensor_samples:
-        sample_batch = torch.load(file)
-        for s in sample_batch:
-            samples += [s.cpu()]
-        if show_progress:
-            pbar.update(1)
-    if show_progress:
-        pbar.close()
-    return torch.cat(samples, 1).permute(1, 0)
