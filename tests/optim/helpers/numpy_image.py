4e2b48544017c22f8d394ff814d613aaccd04225
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-05-22 13:49:22 -0600

    Update numpy_image.py

--- tests/optim/helpers/numpy_image.py
+++ tests/optim/helpers/numpy_image.py
@@ -51,7 +51,9 @@ class FFTImage:
     def rfft2d_freqs(height: int, width: int) -> np.ndarray:
         """Computes 2D spectrum frequencies."""
         fy = np.fft.fftfreq(height)[:, None]
-        fx = np.fft.fftfreq(width)[: width // 2 + 1]
+        # on odd input dimensions we need to keep one additional frequency
+        wadd = 2 if width % 2 == 1 else 1
+        fx = np.fft.fftfreq(width)[: width // 2 + wadd]
         return np.sqrt((fx * fx) + (fy * fy))
 
     def forward(self) -> np.ndarray:
