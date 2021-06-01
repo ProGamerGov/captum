ad9dd7f58e9a6f3bf16a9c25ef9ab5bb0f3d2e53
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-06-01 15:08:21 -0600

    Update vgg.py

--- captum/optim/models/_image/vgg.py
+++ captum/optim/models/_image/vgg.py
@@ -91,7 +91,7 @@ class VGG(nn.Module):
         out_features: int = 1000,
         transform_input: bool = True,
         scale_input: bool = True,
-        classifier_logits: bool = True,
+        classifier_logits: bool = False,
         replace_relus_with_redirectedrelu: bool = False,
         use_linear_modules_only: bool = False,
     ) -> None:
@@ -111,8 +111,8 @@ class VGG(nn.Module):
             pool = nn.MaxPool2d
 
         self.features = _buildSequential(layers, activ, pool)
+        self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
         if self.classifier_logits:
-            self.avgpool = nn.AdaptiveAvgPool2d((7, 7))
             self.classifier = nn.Sequential(
                 nn.Linear(512 * 7 * 7, 4096),
                 activ(),
@@ -139,8 +139,8 @@ class VGG(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self._transform_input(x)
         x = self.features(x)
+        x = self.avgpool(x)
         if self.classifier_logits:
-            x = self.avgpool(x)
             x = torch.flatten(x, 1)
             x = self.classifier(x)
         return x
