ebd5dc3ae47bc000bb17d92b761f65689856155f
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-11-17 18:11:36 -0700

    Update loss.py

--- captum/optim/_core/loss.py
+++ captum/optim/_core/loss.py
@@ -712,7 +712,9 @@ def sum_loss_list(
 
     name = "Sum(" + ", ".join([loss.__name__ for loss in loss_list]) + ")"
     #  Only use unique targets to avoid unnecessary duplication
-    target = list(set([loss.target for loss in loss_list]))
+    #target = list(set([loss.target for loss in loss_list]))
+    targets = [l.target for l in loss_list if not hasattr(l.target, "__iter__") else *l.target]
+    target = list(set(targets))
     return CompositeLoss(loss_fn, name=name, target=target)
 
 
