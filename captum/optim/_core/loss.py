bef25b5f096184689c4ea4978031348a001e500b
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-12-10 07:19:23 -0700

    Update loss.py

--- captum/optim/_core/loss.py
+++ captum/optim/_core/loss.py
@@ -128,8 +128,8 @@ def module_op(
 
         name = f"Compose({', '.join([self.__name__, other.__name__])})"
         target = (
-            self.target if hasattr(self.target, "__iter__") else [self.target]
-        ) + (other.target if hasattr(other.target, "__iter__") else [other.target])
+            self.target if hasattr(self.target, "__iter__") and not isinstance(self.target, torch.jit.ScriptModule) else [self.target]
+        ) + (other.target if hasattr(other.target, "__iter__") and not isinstance(self.target, torch.jit.ScriptModule) else [other.target])
     else:
         raise TypeError(
             "Can only apply math operations with int, float or Loss. Received type "
