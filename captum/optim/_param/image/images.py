3ece9236f8d27c74563854ebd2fe8e37c2c39449
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-12-11 14:47:43 -0700

    Update images.py

--- captum/optim/_param/image/images.py
+++ captum/optim/_param/image/images.py
@@ -19,6 +19,7 @@ from captum.optim._utils.image.common import save_tensor_as_image, show
 TORCH_VERSION = torch.__version__
 
 
+@torch.jit.ignore
 class ImageTensor(torch.Tensor):
     @staticmethod
     def __new__(
