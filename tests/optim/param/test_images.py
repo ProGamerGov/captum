545c92b806f2208bb70702e57ed058ed1375b000
ProGamerGov <ProGamerGov@users.noreply.github.com>
2021-12-27 13:06:44 -0700

    Update test_images.py

--- tests/optim/param/test_images.py
+++ tests/optim/param/test_images.py
@@ -164,12 +164,8 @@ class TestFFTImage(BaseTest):
         spectrum_scale = scale[None, :, :, None]
         assertTensorAlmostEqual(self, image_param.spectrum_scale, spectrum_scale, delta=0.0009)
 
+    @unittest.skipIf(torch.__version__ >= "1.8.0", "Skipping FFTImage fourier_coeffs with init" + " tensor test due to newer Torch version.")
     def test_init_fourier_coeffs_init_tensor_after_v1_7_0(self) -> None:
-        if torch.__version__ >= "1.8.0":
-            raise unittest.SkipTest(
-                "Skipping FFTImage fourier_coeffs with init tensor test due to"
-                + " insufficient Torch version."
-            )
         size = (4, 4)
         init_tensor = torch.ones(1, 3, size[0], size[1])
         image_param = images.FFTImage(init=init_tensor.clone())
@@ -191,6 +187,7 @@ class TestFFTImage(BaseTest):
         assertTensorAlmostEqual(self, image_param.fourier_coeffs, fourier_coeffs)
         self.assertTrue(image_param.fourier_coeffs.requires_grad)
 
+    @unittest.skipUnless(torch.__version__ < "1.7.0", "Skipping FFTImage fourier_coeffs with init" + " tensor test due to newer Torch version.")
     def test_init_fourier_coeffs_init_tensor_before_v1_7_0(self) -> None:
         if torch.__version__ < "1.7.0":
             raise unittest.SkipTest(
