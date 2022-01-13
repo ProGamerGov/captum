aaff8f42c50c81ff7cbf89cc06fbdd904f5284dc
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-01-13 09:04:34 -0700

    Update __init__.py

--- captum/optim/__init__.py
+++ captum/optim/__init__.py
@@ -8,7 +8,7 @@ from captum.optim._param.image.images import ImageTensor  # noqa: F401
 from captum.optim._utils import circuits, reducer  # noqa: F401
 from captum.optim._utils.image import atlas  # noqa: F401
 from captum.optim._utils.image.common import (  # noqa: F401
-    make_image_grid,
+    make_grid_image,
     nchannels_to_rgb,
     save_tensor_as_image,
     show,
@@ -26,7 +26,7 @@ __all__ = [
     "models",
     "reducer",
     "atlas",
-    "make_image_grid",
+    "make_grid_image",
     "nchannels_to_rgb",
     "save_tensor_as_image",
     "show",
