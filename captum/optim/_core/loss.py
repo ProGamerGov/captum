55bdcbb314c4d7be0f60bb2e6cc032ee71bb9c20
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-02-10 13:33:41 -0700

    Update loss.py

--- captum/optim/_core/loss.py
+++ captum/optim/_core/loss.py
@@ -276,7 +276,7 @@ class DeepDream(BaseLoss):
     def __call__(self, targets_to_values: ModuleOutputMapping) -> torch.Tensor:
         activations = targets_to_values[self.target]
         activations = activations[self.batch_index[0] : self.batch_index[1]]
-        return activations**2
+        return activations ** 2
 
 
 @loss_wrapper
@@ -596,7 +596,7 @@ class AngledNeuronDirection(BaseLoss):
             return activations * vec
 
         dot = torch.mean(activations * vec)
-        cossims = dot / (self.eps + torch.sqrt(torch.sum(activations**2)))
+        cossims = dot / (self.eps + torch.sqrt(torch.sum(activations ** 2)))
         return dot * torch.clamp(cossims, min=0.1) ** self.cossim_pow
 
 
