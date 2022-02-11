c89d4e3417fd064a0c7c7a9bffb164e748fd81d1
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-02-11 11:19:38 -0700

    Update _common.py

--- captum/optim/models/_common.py
+++ captum/optim/models/_common.py
@@ -21,9 +21,13 @@ def get_model_layers(model: nn.Module) -> List[str]:
             for name, layer in net._modules.items():
                 if layer is None:
                     continue
-                separator = "" if str(name).isdigit() else "."
-                name = "[" + str(name) + "]" if str(name).isdigit() else name
-                layers.append(separator.join(prefix + [name]))
+                name_str = "".join(
+                    [
+                        ("[" + str(i) + "]" if str(i).isdigit() else "." + i)
+                        for i in prefix + [name]
+                    ]
+                ).strip()
+                layers.append(name_str[1:] if name_str[0] == "." else name_str)
                 get_layers(layer, prefix=prefix + [name])
 
     get_layers(model)
