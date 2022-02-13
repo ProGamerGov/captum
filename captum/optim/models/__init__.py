027e057808969775677bb787231588faeba17a83
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-02-13 12:49:46 -0700

    Update __init__.py

--- captum/optim/models/__init__.py
+++ captum/optim/models/__init__.py
@@ -8,7 +8,7 @@ from ._common import (  # noqa: F401
 )
 from ._image.inception5h_classes import INCEPTION5H_CLASSES  # noqa: F401
 from ._image.inception_v1 import InceptionV1, googlenet  # noqa: F401
-from ._image.clip_resnet50x4 import CLIP_ResNet50x4, clip_resnet50x4_image  # noqa: F401
+from ._image.clip_resnet50x4_image import CLIP_ResNet50x4Image, clip_resnet50x4_image  # noqa: F401
 
 
 __all__ = [
@@ -21,6 +21,6 @@ __all__ = [
     "InceptionV1",
     "googlenet",
     "INCEPTION5H_CLASSES",
-    "CLIP_ResNet50x4",
+    "CLIP_ResNet50x4Image",
     "clip_resnet50x4_image",
 ]
