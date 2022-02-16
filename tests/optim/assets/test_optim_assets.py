4a2bfce72df78480e23dde4a860552806b110da3
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-02-16 09:42:44 -0700

    Create test_optim_assets.py

---
+++ tests/optim/assets/test_optim_assets.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import unittest
+
+import torch
+
+from tests.helpers.basic import BaseTest
+
+
+ASSET_URL = "https://pytorch-tutorial-assets.s3.amazonaws.com/captum/"
+
+
+class TestOptimLoadAssetFiles(BaseTest):
+    def test_load_atlas_m4c_activation_samples(self) -> None:
+        samples = torch.hub.load_state_dict_from_url(
+            url=ASSET_URL + "inceptionv1_mixed4c_relu_samples_activations.pt",
+            progress=False,
+        )
+        self.assertEqual(list(samples.shape), [100000, 512])
+
+    def test_load_atlas_m4c_attribution_samples(self) -> None:
+        samples = torch.hub.load_state_dict_from_url(
+            url=ASSET_URL + "inceptionv1_mixed4c_relu_samples_attributions.pt",
+            progress=False,
+        )
+        self.assertEqual(list(samples.shape), [100000, 1008])
+
+    def test_load_atlas_m5b_activation_samples(self) -> None:
+        samples = torch.hub.load_state_dict_from_url(
+            url=ASSET_URL + "inceptionv1_mixed5b_relu_samples_activations.pt",
+            progress=False,
+        )
+        self.assertEqual(list(samples.shape), [400000, 1024])
+
+    def test_load_atlas_m5b_attribution_samples(self) -> None:
+        samples = torch.hub.load_state_dict_from_url(
+            url=ASSET_URL + "inceptionv1_mixed5b_relu_samples_attributions.pt",
+            progress=False,
+        )
+        self.assertEqual(list(samples.shape), [400000, 1008])
