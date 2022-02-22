82b5837abf7f7c4a3f8651441e946c52d7ef7d95
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-02-22 15:47:37 -0700

    Update clip_resnet50x4_text.py

--- captum/optim/models/_image/clip_resnet50x4_text.py
+++ captum/optim/models/_image/clip_resnet50x4_text.py
@@ -6,7 +6,7 @@ from torch import nn
 
 
 GS_SAVED_WEIGHTS_URL = (
-    "https://pytorch-tutorial-assets.s3.amazonaws.com/captum/clip_resnet50x4_text.pt"
+    "https://pytorch.s3.amazonaws.com/models/captum/clip_resnet50x4_text.pt"
 )
 
 
