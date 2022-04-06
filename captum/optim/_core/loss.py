e3b5ec579cbd4293f206cc0d63dd1d5b4cfb904c
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-04-06 12:28:09 -0600

    Update loss.py

--- captum/optim/_core/loss.py
+++ captum/optim/_core/loss.py
@@ -130,6 +130,8 @@ def module_op(
         target = (
             self.target if hasattr(self.target, "__iter__") else [self.target]
         ) + (other.target if hasattr(other.target, "__iter__") else [other.target])
+        # Filter out duplicate targets
+        target = list(dict.fromkeys(target))
     else:
         raise TypeError(
             "Can only apply math operations with int, float or Loss. Received type "
