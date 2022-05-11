ce2b8cad84521d1902b01793acd6fea054d28279
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-05-11 13:29:11 -0600

    Update test_loss.py

--- tests/optim/core/test_loss.py
+++ tests/optim/core/test_loss.py
@@ -28,10 +28,11 @@ class TestDeepDream(BaseTest):
     def test_channel_deepdream(self) -> None:
         model = BasicModel_ConvNet_Optim()
         loss = opt_loss.DeepDream(model.layer)
+        expected = torch.as_tensor[[[CHANNEL_ACTIVATION_0_LOSS ** 2]], [[CHANNEL_ACTIVATION_1_LOSS ** 2]]])
         assertTensorAlmostEqual(
             self,
-            get_loss_value(model, loss)[None, :],
-            [[[CHANNEL_ACTIVATION_0_LOSS ** 2]], [[CHANNEL_ACTIVATION_1_LOSS ** 2]]],
+            get_loss_value(model, loss),
+            expected,
             mode="max"
         )
 
