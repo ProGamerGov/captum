6f0d43f1445e39b54a69748c37388cb9433ff908
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-05-11 13:22:57 -0600

    Update test_output_hook.py

--- tests/optim/core/test_output_hook.py
+++ tests/optim/core/test_output_hook.py
@@ -11,10 +11,6 @@ from tests.helpers.basic import BaseTest
 
 class TestActivationFetcher(BaseTest):
     def test_activation_fetcher(self) -> None:
-        if torch.__version__ <= "1.2.0":
-            raise unittest.SkipTest(
-                "Skipping ActivationFetcher test due to insufficient Torch version."
-            )
         model = googlenet(pretrained=True)
 
         catch_activ = output_hook.ActivationFetcher(model, targets=[model.mixed4d])
