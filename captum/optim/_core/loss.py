4e022277d1a29f526abd0955118cb911263e04d4
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-05-16 13:33:09 -0600

    Add files via upload

--- captum/optim/_core/loss.py
+++ captum/optim/_core/loss.py
@@ -5,7 +5,11 @@ from typing import Any, Callable, List, Optional, Tuple, Union
 
 import torch
 import torch.nn as nn
-from captum.optim._utils.image.common import _create_new_vector, _dot_cossim, get_neuron_pos
+from captum.optim._utils.image.common import (
+    _create_new_vector,
+    _dot_cossim,
+    get_neuron_pos,
+)
 from captum.optim._utils.typing import ModuleOutputMapping
 
 
