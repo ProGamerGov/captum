bd3be4166ea60534d6b9ab8fd57546224903893e
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-06-27 09:38:09 -0600

    Update images.py

--- captum/optim/_param/image/images.py
+++ captum/optim/_param/image/images.py
@@ -32,10 +32,10 @@ class ImageTensor(torch.Tensor):
         Args:
 
             x (list or np.ndarray or torch.Tensor): A list, NumPy array, or PyTorch
-                tensor to create an `ImageTensor` from.
+                tensor to create an ``ImageTensor`` from.
 
         Returns:
-           x (ImageTensor): An `ImageTensor` instance.
+           x (ImageTensor): An ``ImageTensor`` instance.
         """
         if isinstance(x, torch.Tensor) and x.is_cuda:
             x.show = MethodType(cls.show, x)
@@ -45,17 +45,18 @@ class ImageTensor(torch.Tensor):
             return super().__new__(cls, x, *args, **kwargs)
 
     @classmethod
-    def open(cls, path: str, scale: float = 255.0, mode: str = "RGB") -> "ImageTensor":
+    def load(cls, path: str, scale: float = 255.0, mode: str = "RGB") -> "ImageTensor":
         """
-        Load an image file from a URL or local filepath directly into an `ImageTensor`.
+        Load an image file from a URL or local filepath directly into an
+        ``ImageTensor``.
 
         Args:
 
             path (str): A URL or filepath to an image.
             scale (float, optional): The image scale to use.
-                Default: 255.0
+                Default: ``255.0``
             mode (str, optional): The image loading mode / colorspace to use.
-                Default: "RGB"
+                Default: ``"RGB"``
 
         Returns:
            x (ImageTensor): An `ImageTensor` instance.
@@ -70,8 +71,8 @@ class ImageTensor(torch.Tensor):
         return cls(img_np.transpose(2, 0, 1) / scale)
 
     @classmethod
-    def load(cls, path: str, scale: float = 255.0, mode: str = "RGB") -> "ImageTensor":
-        """Alias of ImageTensor.open()"""
+    def open(cls, path: str, scale: float = 255.0, mode: str = "RGB") -> "ImageTensor":
+        r"""Alias for :func:`load`."""
         return cls.open(path=path, scale=scale, mode=mode)
 
     def __repr__(self) -> str:
@@ -109,25 +110,25 @@ class ImageTensor(torch.Tensor):
         pad_value: float = 0.0,
     ) -> None:
         """
-        Display an `ImageTensor`.
+        Display an ``ImageTensor`` instance.
 
         Args:
 
             figsize (Tuple[int, int], optional): height & width to use
-                for displaying the `ImageTensor` figure.
-            scale (float, optional): Value to multiply the `ImageTensor` by so that
+                for displaying the ``ImageTensor`` figure.
+            scale (float, optional): Value to multiply the ``ImageTensor`` by so that
                 it's value range is [0-255] for display.
-                Default: 255.0
+                Default: ``255.0``
             images_per_row (int, optional): The number of images per row to use for the
-                grid image. Default is set to None for no grid image creation.
-                Default: None
+                grid image. Default is set to ``None`` for no grid image creation.
+                Default: ``None``
             padding (int, optional): The amount of padding between images in the grid
-                images. This parameter only has an effect if `images_per_row` is not
-                None.
-                Default: 2
+                images. This parameter only has an effect if ``images_per_row`` is not
+                ``None``.
+                Default: ``2``
             pad_value (float, optional): The value to use for the padding. This
-                parameter only has an effect if `images_per_row` is not None.
-                Default: 0.0
+                parameter only has an effect if ``images_per_row`` is not None.
+                Default: ``0.0``
         """
         show(
             self,
@@ -152,23 +153,24 @@ class ImageTensor(torch.Tensor):
 
         Args:
 
-            filename (str): The filename to use when saving the `ImageTensor` as an
+            filename (str): The filename to use when saving the ``ImageTensor`` as an
                 image file.
-            scale (float, optional): Value to multiply the `ImageTensor` by so that
+            scale (float, optional): Value to multiply the ``ImageTensor`` by so that
                 it's value range is [0-255] for saving.
-                Default: 255.0
+                Default: ``255.0``
             mode (str, optional): A PIL / Pillow supported colorspace. Default is
                 set to None for automatic RGB / RGBA detection and usage.
-                Default: None
+                Default: ``None``
             images_per_row (int, optional): The number of images per row to use for the
                 grid image. Default is set to None for no grid image creation.
-                Default: None
+                Default: ``None``
             padding (int, optional): The amount of padding between images in the grid
-                images. This parameter only has an effect if `images_per_row` is not
-                None.
+                images. This parameter only has an effect if ``images_per_row`` is not
+                ``None``.
+                Default: ``2``
             pad_value (float, optional): The value to use for the padding. This
-                parameter only has an effect if `images_per_row` is not None.
-                Default: 0.0
+                parameter only has an effect if ``images_per_row`` is not ``None``.
+                Default: ``0.0``
         """
         save_tensor_as_image(
             self,
