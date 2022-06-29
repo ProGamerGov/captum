3fd2d498e71bbc4b5babe81ad697cc6408078980
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-06-28 19:58:30 -0600

    Update transforms.py

--- captum/optim/_param/image/transforms.py
+++ captum/optim/_param/image/transforms.py
@@ -77,6 +77,9 @@ class ToRGB(nn.Module):
     We offer two precalculated transforms: Karhunen-Loève (KLT) and I1I2I3.
     KLT corresponds to the empirically measured channel correlations on imagenet.
     I1I2I3 corresponds to an approximation for natural images from Ohta et al.[0]
+    If you wish to calculate your own KLT transform matrix on a custom dataset,
+    then see :func:`captum.optim.dataset.dataset_klt_matrix` for an example
+    of how to do so.
 
     [0] Y. Ohta, T. Kanade, and T. Sakai, "Color information for region segmentation,"
     Computer Graphics and Image Processing, vol. 13, no. 3, pp. 222–241, 1980
@@ -87,10 +90,7 @@ class ToRGB(nn.Module):
         >>> to_rgb = opt.transforms.ToRGB()
         >>> x = torch.randn(1, 3, 224, 224)
         >>> decorrelated_colors = to_rgb(x, inverse=True)
-        >>> recorrelated_colors = to_rgb(decorrelated_colors, inverse=True)
-    
-    See :func:`captum.optim.dataset.dataset_klt_matrix` for an example of how to create
-    your own color correlation transform matrix.
+        >>> recorrelated_colors = to_rgb(decorrelated_colors)
     """
 
     @staticmethod
