f09f17ab98b53fa26a3587123bc706a420986509
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-07-01 11:09:17 -0600

    Update __init__.py

--- captum/__init__.py
+++ captum/__init__.py
@@ -2,15 +2,7 @@
 import captum.attr as attr  # noqa
 import captum.concept as concept  # noqa
 import captum.influence as influence  # noqa
-
-try:
-    import captum.insights as insights  # noqa
-except (ImportError, AssertionError):
-    print(
-        "The {} libraries are required if using Captum's Insights module".format(
-            ["flask", "ipython", "ipywidgets", "jupyter", "flask-compress"]
-        )
-    )
+import captum.insights as insights  # noqa
 import captum.log as log  # noqa
 import captum.metrics as metrics  # noqa
 import captum.robust as robust  # noqa
