2188eb84cdd881b93bab5d2c5fd424a54419c488
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-07-01 11:21:37 -0600

    Update __init__.py

--- captum/insights/__init__.py
+++ captum/insights/__init__.py
@@ -3,8 +3,9 @@ from warnings import warn
 try:
     from captum.insights.attr_vis import AttributionVisualizer, Batch  # noqa
 except (ImportError, AssertionError) as e:
-    warning_msg = "Missing required libraries for Captum's Insights module." \
-        + " This warning can be ignored if not using Insights." \
+    warning_msg = (
+        "Missing required libraries for Captum's Insights module."
+        + " This warning can be ignored if not using Insights."
         + " {}".format(e)
-
+    )
     warn(warning_msg)
