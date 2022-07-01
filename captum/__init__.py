f54716e15fd5947797c828f0d0a997839241f599
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-07-01 10:56:34 -0600

    Fix dependency error

--- captum/__init__.py
+++ captum/__init__.py
@@ -2,7 +2,15 @@
 import captum.attr as attr  # noqa
 import captum.concept as concept  # noqa
 import captum.influence as influence  # noqa
-import captum.insights as insights  # noqa
+
+try:
+    import captum.insights as insights  # noqa
+except (ImportError, AssertionError):
+    print(
+        "The {} libraries are required if using Captum's Insights module".format(
+            ["flask", "ipython", "ipywidgets", "jupyter", "flask-compress"]
+        )
+    )
 import captum.log as log  # noqa
 import captum.metrics as metrics  # noqa
 import captum.robust as robust  # noqa
