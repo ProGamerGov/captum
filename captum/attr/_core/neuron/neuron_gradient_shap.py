2b6672392701d36a2abe2f4629e80a78e8e2681a
ProGamerGov <ProGamerGov@users.noreply.github.com>
2022-07-26 14:54:03 -0600

    Update neuron_gradient_shap.py

--- captum/attr/_core/neuron/neuron_gradient_shap.py
+++ captum/attr/_core/neuron/neuron_gradient_shap.py
@@ -135,7 +135,7 @@ class NeuronGradientShap(NeuronAttribution, GradientAttribution):
                           indexed output tensor is used for attribution. Note
                           that specifying a slice of a tensor would amount to
                           computing the attribution of the sum of the specified
-                          neurons, and not the individual neurons independantly.
+                          neurons, and not the individual neurons independently.
 
                         - a callable, which should
                           take the target layer as input (single tensor or tuple
